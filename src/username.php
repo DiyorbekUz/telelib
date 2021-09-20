3adbfdc616990278ded806d513b73a34fe0779b7
DiyorbekUz <diyorbekermamatov@gmail.com>
2021-09-20 01:06:28 -0400

    all functions

--- src/username.php
+++ src/username.php
@@ -21,16 +21,8 @@ class Username
     public const URL_FORMAT_TELE_CLICK = 'https://tele.click/%s';
     public const URL_FORMAT_DEFAULT = self::URL_FORMAT_T_ME;
 
-    /**
-     * @var string
-     */
     protected string $username;
 
-    /**
-     * Username constructor.
-     *
-     * @param string $username
-     */
     public function __construct(string $username)
     {
         $username = ltrim($username, '@');
@@ -40,10 +32,6 @@ class Username
         $this->username = $username;
     }
 
-    /**
-     * @param string $username
-     * @return bool
-     */
     public static function checkUsername(string $username): bool
     {
         return strpos($username, '__') === false &&
@@ -51,42 +39,27 @@ class Username
 //            && preg_match('/_$/', $username) === false;
     }
 
-    /**
-     * @return string
-     */
+
     public function getLower(): string
     {
         return strtolower($this->username);
     }
 
-    /**
-     * @return string
-     */
     public function getUpper(): string
     {
         return strtoupper($this->username);
     }
 
-    /**
-     * @return string
-     */
     public function getShort(): string
     {
         return $this->username;
     }
 
-    /**
-     * @return string
-     */
     public function getFull(): string
     {
         return '@' . $this->username;
     }
 
-    /**
-     * @param string $url_format
-     * @return string
-     */
     public function getUrl(string $url_format = self::URL_FORMAT_DEFAULT): string
     {
         return sprintf($url_format, $this->username);
